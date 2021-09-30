862491b975a2410b31e01afe86bfb05392804b75
AswathyPramod <81959864+AswathyPramod@users.noreply.github.com>
2021-09-30 20:15:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+/*tety
+thrfj*/
+
+function changecolor(){
+    var element;
+    element = document.getElementById("heading");
+   
+    
+}
+if(element.style.color =="black"){
+    element.style.color = "blue";
+
+}
+else{
+    element.style.color = "black";
+
+}
